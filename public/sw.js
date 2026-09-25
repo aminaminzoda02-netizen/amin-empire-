4c18d903c35f8c6ab5774bd382b5ca2ba4934b2d
aminaminzoda02-netizen <aminaminzoda02@gmail.com>
2026-09-25 18:12:45 +0500

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+const CACHE='amin-empire-v1';
+const CORE=['/','/index.html','/mobile.html','/manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
